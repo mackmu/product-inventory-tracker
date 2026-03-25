5bdfcb07d07c506317783179a3ff8486057418aa
Dylan Wintemute <djwintemute@students.ntc.edu>
2026-03-25 16:45:34 -0500

    S26PBI1198 feat: added test for product class

---
+++ ProductInventoryTracker/ProductInventoryTracker.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductInventoryTracker.Tests
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        public void CalculateSubtotal()
+        {
+            var p = new Product(1, "test", 3.14m, 99, 1);
+
+            Assert.AreEqual(310.86m, p.Subtotal);
+        }
+    }
+}
